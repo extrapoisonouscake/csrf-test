ad96278bc5dbc72322e50b1efa7c68eb79c0647b
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-05-30 14:41:36 -0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { csrf } from "@/instances/csrf";
 import { Form } from "./form";
-
+export const dynamic = "force-dynamic";
 export default function Home() {
   const csrfToken = csrf.create(process.env.CSRF_TOKEN_SECRET!);
   return <Form csrfToken={csrfToken} />;
